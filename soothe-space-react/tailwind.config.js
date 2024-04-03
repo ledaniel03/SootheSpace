b1e591f5d9971fd230f7591ef1fbf7ae7018ba07
dhldaniel03@gmail.com <dhldaniel03@gmail.com>
2024-04-03 18:12:32 -0400

    Tailwind CSS setup

---
+++ soothe-space-react/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // all js & jsx files in src directory & subdirectories
+  content: ["./src/**/*.{js,jsx}",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
