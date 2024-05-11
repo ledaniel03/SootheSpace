515d1f1795835cf4ffe93d0e5f37012d426b4b1a
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2024-05-11 16:13:44 -0400

    Delete soothespace_backend/breathing_tools directory

--- soothespace_backend/breathing_tools/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BreathingToolsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'breathing_tools'
--- soothespace_backend/breathing_tools/urls.py
+++
@@ -1,8 +0,0 @@
-# breathing_tools/urls.py
-from django.urls import path
-from .views import MeditationVideoList, complete_meditation
-
-urlpatterns = [
-    path('meditations/<str:category>/<int:duration>/', MeditationVideoList.as_view(), name='meditation-videos'),
-    path('complete-meditation/', complete_meditation, name='complete_meditation'),
-]
